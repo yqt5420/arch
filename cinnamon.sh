b221f58be8b0a07a3cbf9afe68c039efdaa12a26
yqt5420 <54099324+yqt5420@users.noreply.github.com>
2024-08-07 16:35:57 +0800

    Create cinnamon.sh

---
+++ cinnamon.sh
@@ -0,0 +1,10 @@
+yay -S --needed --noconfirm xorg
+###cinnamon桌面安装
+yay -S --needed --noconfirm cinnamon lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings 
+
+#开启语言包设置支持
+yay -S --needed --noconfirm cinnamon-translations mintlocale
+
+#主题相关的包
+yay -S --needed --noconfirm mint-themes mint-x-icons mint-y-icons mint-backgrounds
+
